6e85dfb2479e9fe29b280df0af3d5616f6f3b57b
Ali Rahman <amrahman1996@gmail.com>
2018-01-17 20:57:46 -0500

    Made bots work more realistically

--- Assets/SwarmDrone/Scripts/SwarmDrone.cs
+++ Assets/SwarmDrone/Scripts/SwarmDrone.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwarmDrone : MonoBehaviour {
+public class SwarmDrone : MonoBehaviour{
     #region Variables
+    public bool isControlledBySwarm = true;
+    public float fwdControl, turnControl;
+    public List<WheelCollider> wheelsR, wheelsL;
+    public MeshRenderer headTexture;
     public float maxMoveSpeed = 20f;
     public float kick = 20;
     public float acceleration = 30f;
@@ -12,47 +16,38 @@ public class SwarmDrone : MonoBehaviour {
     float velForward, velRight;
     Vector3 fwdVec, rightVec;
     Vector3 tangentVec, tangentRightVec;
+
     #endregion
 
-	// Use this for initialization
-	void Start () {
+    void Start(){
         rb = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        Turn(-1f);
-        Move(.5f);
-	}
+        headTexture.material.color = Color.HSVToRGB(Random.Range(0, 360) / 360f, Random.Range(20, 100) / 100f, Random.Range(50, 100) / 100f);
+    }
 
-    /// <summary>
-    /// Moves the bot forwards or backwards
-    /// </summary>
-    /// <param name="fwdInput">Float between -1 and 1 that controls bots forward movement</param>
-    void Move(float fwdInput){
-        velForward = Vector3.Dot(rb.velocity, transform.forward); //Current fwd speed. Used to see if Kick is necessary.
-        velRight = Vector3.Dot(rb.velocity, transform.right);//Similar to ^
-
-        fwdVec = transform.forward * acceleration * fwdInput * Time.deltaTime / 50 * rb.mass;
-        if(Mathf.Abs(fwdInput) > 0 && Mathf.Abs(velForward) < maxMoveSpeed){
-            rb.velocity += fwdVec;
-        }
-
-        //::Kick - If moving from standstill, gives a kick so moving is more responsive.
-        if(fwdInput > 0 && velForward < maxMoveSpeed / 3){
-            rb.AddRelativeForce(0, 0, kick / 10 * rb.mass);
-            //Debug.Log("kickf"); //\\\\\\\\\\\\\\\\\\\\\\\\\\
-        }else if(fwdInput < 0 && velForward > -maxMoveSpeed / 3){
-            rb.AddRelativeForce(0, 0, -kick / 10 * rb.mass);
-            //Debug.Log("kickb"); //\\\\\\\\\\\\\\\\\\\\\\\\\\
-        }
+    void FixedUpdate(){
+        if(isControlledBySwarm)
+            FlockingControl();
+        
+        Move(fwdControl, turnControl);
     }
 
     /// <summary>
-    /// Turns the bot left or right
+    /// Sets fwdControl and turnContol based on flocking algorithm.
     /// </summary>
+    void FlockingControl(){
+        //
+    }
+
+
+    /// <summary>
+    /// Controls the bot movement. Should only be called once per physics timestep (last call will always override).
+    /// </summary>
+    /// <param name="fwdInput">Float between -1 and 1 that controls bots forward movement</param>
     /// <param name="turnInput">Float between -1 and 1 specifying how fast to turn. 1 is full right, -1 full left.</param>
-    void Turn(float turnInput){
-        rb.AddRelativeTorque(0f, turnInput * turnSpeed, 0f, ForceMode.Acceleration);
+    void Move(float fwdInput, float turnInput){
+        foreach(WheelCollider wheel in wheelsR)
+            wheel.motorTorque = fwdInput * acceleration - turnInput * turnSpeed;
+        foreach(WheelCollider wheel in wheelsL)
+            wheel.motorTorque = fwdInput * acceleration + turnInput * turnSpeed;
     }
 }
